4501b1f038855193c4f78146048779fb9ba6c1b6
mattostraszewski <mrostraszewski@gmail.com>
2020-05-13 15:03:06 -0600

    App2

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      skydivesDisciplines: ['CRW', 'Freefly', 'Wingsuit', 'XRW', 'Belly', 'Tracking', 'Swooping']
+    }
+  }
+
+  render() {
+    const displayDisciplines = this.state.skydivesDisciplines.map((el, ind) => {
+      return <h2 key={ind}>{el}</h2>
+    })
+
+    return (
+      <div className='App'>{displayDisciplines}</div>
+    )
+  }
+
 }
 
 export default App;
