aa5f3b737c7005d6458a4f06d6321a9631e7e71c
mattostraszewski <mrostraszewski@gmail.com>
2020-05-13 15:57:59 -0600

    App3

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      skydivesDisciplines: ['CRW', 'Freefly', 'Wingsuit', 'XRW', 'Belly', 'Tracking', 'Swooping'],
+      userInput: '',
+
+    }
+  }
+
+  textInput(value) {
+    this.setState({
+      userInput: value
+    })
+  }
+
+
+  render() {
+    const displayDisciplines = this.state.skydivesDisciplines
+      .filter((element) => {
+        return element.includes(this.state.userInput);
+      })
+      .map((el, ind) => {
+        return <h2 key={ind}>{el}</h2>
+      })
+
+    return (
+      <div className='App'>
+
+        <input onChange={event => this.textInput(event.target.value)} />
+
+        <div>{displayDisciplines}</div>
+
+      </div>
+    )
+  }
+
 }
 
 export default App;
