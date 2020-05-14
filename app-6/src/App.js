1c89cf28bf5c59c88e873fd7aae1cd7ff1c0b893
mattostraszewski <mrostraszewski@gmail.com>
2020-05-13 23:22:31 -0600

    App6

--- app-6/src/App.js
+++ app-6/src/App.js
@@ -1,58 +1,41 @@
 import React, { Component } from 'react';
 import './App.css';
 import Todo from './Todo.js';
-
 export default class App extends Component {
-
   constructor() {
     super();
-
     this.state = {
-      text: '',
-      list: []
+      list: [],
+      text: ''
     }
-
   }
-
-
-  textInput(value) {
+  textInput = (value) => {
+    console.log(value)
     this.setState({
       text: value
     })
   }
-
-  AddToArray = () => {
+  addToArray = () => {
     this.setState({
-      list: [...this.state.list, this.state.input], //dont know what this line of code is doing.
-      input: ''
+      list: [...this.state.list, this.state.text], //dont know what this line of code is doing.
+      text: ''
     })
   }
-
-
   render() {
-
     let list = this.state.list.map((element, index) => {
       return <Todo key={index} task={element} />;    //do not understand why this is needed.
     })
-
     return (
-
       <div className='App'>
         <h1>My to-do list:</h1>
-
         <input
-          value={this.state.input} //need explanation please.
+          value={this.state.text} //need explanation please.
           placeholder='Enter new task'
           onChange={e => this.textInput(e.target.value)}
         />
-
-        <button onClick={this.AddToArray}>Add</button>
-
-
-
+        <button onClick={this.addToArray}>Add</button>
+        <div>{list}</div>
       </div>
-
     )
-
   }
 }
