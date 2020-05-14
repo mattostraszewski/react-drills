f4df3bed7cd101cf9d1e59b89794ae4b812f8467
mattostraszewski <mrostraszewski@gmail.com>
2020-05-13 19:06:07 -0600

    App5

--- app-5/src/App.js
+++ app-5/src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Image from './Image'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  // constructor() {
+  //   super();
+
+  //   this.state = {    why doesnt it need this?
+
+  //   }
+  // }
+
+  render() {
+
+    return (
+
+      <div className='App'>
+
+        <Image url={'https://skydiveperris.com/b/wp-content/uploads/2018/08/angle-flying-coach.jpg'} />
+
+
+      </div>
+
+    )
+
+  }
+
+
 }
 
 export default App;
