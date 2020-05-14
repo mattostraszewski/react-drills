650219c66412887b950355155fae7b97b4bd62fd
mattostraszewski <mrostraszewski@gmail.com>
2020-05-13 20:53:28 -0600

    App6

--- app-6/src/App.js
+++ app-6/src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Todo from './Todo.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      text: '',
+      list: []
+    }
+
+  }
+
+
+  textInput(value) {
+    this.setState({
+      text: value
+    })
+  }
+
+  AddToArray = () => {
+    this.setState({
+      list: [...this.state.list, this.state.input], //dont know what this line of code is doing.
+      input: ''
+    })
+  }
+
+
+  render() {
+
+    let list = this.state.list.map((element, index) => {
+      return <Todo key={index} task={element} />;    //do not understand why this is needed.
+    })
+
+    return (
+
+      <div className='App'>
+        <h1>My to-do list:</h1>
+
+        <input
+          value={this.state.input} //need explanation please.
+          placeholder='Enter new task'
+          onChange={e => this.textInput(e.target.value)}
+        />
+
+        <button onClick={this.AddToArray}>Add</button>
+
+
+
+      </div>
+
+    )
+
+  }
 }
-
-export default App;
